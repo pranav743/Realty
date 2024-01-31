63974d481b4186082dbdf62323f1a09f65be50aa
Vishal Patil <vishalpatil6924@gmail.com>
2024-01-31 18:22:54 +0530

    blockchaincalls

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -15,6 +15,56 @@ import { getUserDetails } from "../../Global/authUtils";
 import Loader from "../../components/Loader";
 
 const Home = () => {
+   
+  const [blockdata,setblockdata]=useState();
+
+  //BLOCKCHAIN CALL STARTS
+
+  const [state, setState] = useState({
+    provider: null,
+    signer: null,
+    contract: null,
+  });
+
+
+
+  useEffect(() => {
+    const connectWallet = async () => {
+      const contractAddress = "0xc0be1A1d46A7740d9F31F9EFD19d5E45CDb0c2F6";
+      const contractAbi = abi.abi; 
+      try {
+        const { ethereum } = window;
+        if (ethereum) {
+          const account = await ethereum.request({ method: "eth_requestAccounts" });
+        } else {
+          console.log("no metamask");
+        }
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const contract = new ethers.Contract(contractAddress, contractAbi, signer);
+        setState({ provider, signer, contract });
+      } catch (error) {
+        console.log(error);
+      }
+    };
+     
+    const getblockdata = async()=>{
+      try {
+        const Bdata = await contract.allListedProperties();
+        setblockdata(Bdata)
+        alert("DATA IS Coming");
+      } catch (error) {
+        console.error("Error fetching batch details:", error);
+      }
+    }
+    
+    getblockdata();
+    connectWallet();
+  }, []);
+
+
+
+  //BLOCKCHAIN CALL ENDS
   const [user, setUser] = useState(false);
   const navigate = useNavigate();
 
