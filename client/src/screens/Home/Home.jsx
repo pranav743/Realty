76d479dea687fc772fff5ab3cf6de6ba6af620f7
Pranav Patil <pranav07@somaiya.edu>
2024-01-31 12:14:43 +0530

    connected properties with database

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
-import { getUserDetails } from "../../Global/authUtils";
 
 // importing components
 import Promotion from "./Promotion";
@@ -8,9 +7,28 @@ import HomeSubMenu from "./HomeSubMenu";
 import PropertyFilter from "./PropertyFilter";
 import AllLocations from "./AllLocations";
 
+// Requesting
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
+import { url } from "../../Global/URL";
+import { getUserDetails } from "../../Global/authUtils";
+import Loader from "../../components/Loader";
+
 const Home = () => {
   const [user, setUser] = useState(false);
   const navigate = useNavigate();
+
+  const { isError, isLoading, data } = useQuery({
+    queryKey: ['/properties/all'],
+    retryDelay: 10000,
+    queryFn: async () => {
+      const temp = await axios
+        .get(url + `/properties/all`)
+        .then((response) => response.data.data);
+      console.log(temp);
+      return temp;
+    },
+  });
 
   const getData = async () => {
     try {
@@ -27,6 +45,17 @@ const Home = () => {
   useEffect(() => {
     // getData();
   }, []);
+
+  if (isLoading) {
+    return (
+      <Loader/>
+    )
+  }
+  if (isError) {
+    return (
+      <h1>Something Wen't Wrong :(</h1>
+    )
+  }
   return (
     <div>
       <Promotion />
